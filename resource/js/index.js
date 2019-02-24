709c8dc99cd98ea651ba5cd981ebcc31ca69989c
Nick <nickmw.tan@gmail.com>
2019-02-24 16:31:53 +0800

    Initial skeleton
    Added basic front end, scripts & css

---
+++ resource/js/index.js
@@ -0,0 +1,12 @@
+$('.sub-menu-tab').slick({
+  dots: false,
+  arrows: false,
+  infinite: false,
+  slidesToShow: 1,
+  // slidesToScroll: 3,
+  centerMode: false,
+  swipeToSlide: true,
+  variableWidth: true
+});
+
+
